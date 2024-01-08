8c616c6eedfd11b5de3a5a3fb8ff5d371a299a0e
MUG <ys8267928563@outlook.jp>
2024-01-08 17:49:08 +0900

    進行度の更新
    Nextボタンを押す毎に数値変動

--- Assets/Scripts/Quest/QuestPresenter.cs
+++ Assets/Scripts/Quest/QuestPresenter.cs
@@ -1,10 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class QuestPresenter : MonoBehaviour
 {
+    [SerializeField]
+    TextMeshProUGUI stageText;
+
+    private int curretStage = 0;
+
+    void Start()
+    {
+        stageText.text = string.Format("Stage {0}", curretStage);
+    }
+
+    public void OnNextButton()
+    {
+        curretStage++;
+        stageText.text = string.Format("Stage {0}", curretStage);
+    }
+
     public void OnBackButton()
     {
         SceneManager.LoadScene("Quest");
